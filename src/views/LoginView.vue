78d9fd606b44b926c3ef43c84f5e5b025c7295dd
EstherAzevedo <estherazvdo123@gmail.com>
2026-04-03 21:57:52 -0300

    feat: logo da tela mobile e responsividade

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
-import logo from '../assets/logo_vertical_bg_dark.svg'
+import logoBgDark from '../assets/logo_vertical_bg_dark.svg'
+import logoBgWhite from '../assets/logo_horizontal_bg_white.svg'
 import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import { loginSchema } from '@/validation/loginSchema'
@@ -35,10 +36,10 @@ const handleLogin = handleSubmit(async (values) => {
 </script>
 
 <template>
-  <div class="flex h-screen font-sans">
+  <div class="flex h-screen flex-col md:flex-row min-h-screen font-sans">
 
     <!-- LEFT -->
-    <div class="flex-1 relative overflow-hidden bg-white">
+    <aside class="hidden md:flex flex-1 relative overflow-hidden bg-white">
 
       <svg viewBox="0 0 1080 1080" preserveAspectRatio="none" class="absolute inset-0 w-full h-full object-cover">
         <path d="M0 0H1012.06C1239.52 336 805.478 728 1012.06 1080H0V0Z" fill="url(#paint0_linear_4_655)" />
@@ -54,7 +55,7 @@ const handleLogin = handleSubmit(async (values) => {
       <div class="relative z-10 flex flex-col justify-center items-start h-full px-20 text-white">
         <div class="flex flex-col items-start gap-6 max-w-md">
 
-          <img :src="logo" alt="logo" class="w-[260px]" />
+          <img :src="logoBgDark" alt="logo" class="w-[260px]" />
           <div>
             <h2 class="text-3xl font-semibold mb-2">
               Bem-vindo de volta
@@ -66,21 +67,28 @@ const handleLogin = handleSubmit(async (values) => {
           </div>
         </div>
       </div>
-    </div>
+    </aside>
 
-      <!-- RIGHT -->
-      <div class="flex-1 flex justify-center items-center bg-white">
+    <!-- RIGHT -->
+    <main class="flex-1 flex justify-center items-center bg-white px-6">
 
-        <main class="pt-10 pb-20 px-8 w-[350px] h-[430px] rounded-2xl shadow-xl border border-red-400">
+      <div class="flex flex-col items-center w-full">
+
+        <div class="mb-6 md:hidden">
+          <img :src="logoBgWhite" alt="Logo do StaReview" class="w-[180px]" />
+        </div>
+
+        <div class="pt-10 pb-10 px-6 w-full max-w-[350px] rounded-2xl shadow-xl border border-red-400">
 
           <h2 class="flex text-xl font-semibold font-karla text-[#292929] justify-center mb-6">
             Entrar
           </h2>
-
+          
           <form class="space-y-4" @submit.prevent="handleLogin" novalidate>
             <div class="flex flex-col">
               <label for="email" class="text-sm mb-1 font-inconsolata text-gray-700 pl-3"> Email</label>
-              <input id="email" v-model="email" v-bind="emailAttrs" type="email" :aria-invalid="!!errors.email" aria-describedby="email-error" placeholder="seuemail@email.com" 
+              <input id="email" v-model="email" v-bind="emailAttrs" type="email" :aria-invalid="!!errors.email"
+                aria-describedby="email-error" placeholder="seuemail@email.com"
                 class="p-3 rounded-[25px] border bg-green-50 py-2 focus:outline-none" />
 
               <p class="text-sm text-red-600 mt-2">
@@ -91,7 +99,8 @@ const handleLogin = handleSubmit(async (values) => {
             <div class="flex flex-col">
               <label for="password" class="text-sm mb-1 font-inconsolata text-gray-700 pl-3"> Senha</label>
 
-              <input id="password" v-model="password" v-bind="passwordAttrs" type="password" placeholder="********" autocomplete="current-password"
+              <input id="password" v-model="password" v-bind="passwordAttrs" type="password" placeholder="********"
+                autocomplete="current-password"
                 class="w-full p-3 rounded-[25px] border bg-green-50 py-2 focus:outline-none" />
 
               <p class="text-sm text-red-600 mt-2">
@@ -111,7 +120,8 @@ const handleLogin = handleSubmit(async (values) => {
               </router-link>
             </p>
           </form>
-        </main>
+        </div>
       </div>
-    </div>
+    </main>
+  </div>
 </template>
