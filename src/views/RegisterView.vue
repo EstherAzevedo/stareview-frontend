47d9d9b1fc7ee2d8a44e7480b404b486c65f9712
EstherAzevedo <estherazvdo123@gmail.com>
2026-04-04 10:56:33 -0300

    feat: mudança do design de registerView

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue'
+import logoBgDark from '../assets/logo_vertical_bg_dark.svg'
+import logoBgWhite from '../assets/logo_horizontal_bg_white.svg'
 import { useRouter } from 'vue-router'
 import logo from '../assets/logo-star-antigo.png'
 import { useForm } from 'vee-validate'
@@ -20,18 +22,18 @@ const { handleSubmit, errors, defineField, resetForm } = useForm({
 })
 
 const [name, nameAttrs] = defineField('name', {
-  validateOnModelUpdate: true 
+  validateOnModelUpdate: true
 })
 
 const [email, emailAttrs] = defineField('email', {
-  validateOnModelUpdate: true 
+  validateOnModelUpdate: true
 })
 const [password, passwordAttrs] = defineField('password', {
-  validateOnModelUpdate: true 
+  validateOnModelUpdate: true
 })
 
 const [confirmPassword, confirmPasswordAttrs] = defineField('confirmPassword', {
-  validateOnModelUpdate: true 
+  validateOnModelUpdate: true
 })
 
 const handleRegister = handleSubmit(async (values) => {
@@ -47,122 +49,112 @@ const handleRegister = handleSubmit(async (values) => {
 </script>
 
 <template>
-  
-  <div class="flex h-screen font-sans">
+
+  <div class="flex h-screen flex-col md:flex-row min-h-screen font-sans">
 
     <!-- LEFT -->
-    <div class="flex-1 bg-gradient-to-br from-[#6B1E3D] to-[#7E57C2] text-white flex flex-col justify-center px-20">
-      
-      <div class="flex items-center gap-4 mb-10">
-        <img :src="logo" alt="logo" class="w-14" />
-        <h1 class="text-2xl font-semibold">StaReview</h1>
-      </div>
+    <aside class="hidden md:flex flex-1 relative overflow-hidden bg-white">
 
-      <div>
-        <h2 class="text-2xl font-semibold mb-4">
-          Comece sua jornada literária ✨
-        </h2>
-        <p class="max-w-md leading-relaxed text-sm opacity-90">
-          Crie sua conta para avaliar livros, escrever resenhas
-          e compartilhar suas leituras favoritas.
-        </p>
-      </div>
+      <svg viewBox="0 0 1080 1080" preserveAspectRatio="none" class="absolute inset-0 w-full h-full object-cover">
+        <path d="M0 0H1012.06C1239.52 336 805.478 728 1012.06 1080H0V0Z" fill="url(#paint0_linear_4_655)" />
+        <defs>
+          <linearGradient id="paint0_linear_4_655" x1="1012.06" y1="1080" x2="47.3168" y2="-40.7312"
+            gradientUnits="userSpaceOnUse">
+            <stop stop-color="#CF5768" />
+            <stop offset="1" stop-color="#880D1E" />
+          </linearGradient>
+        </defs>
+      </svg>
 
-    </div>
+      <div class="relative z-10 flex flex-col justify-center items-start h-full px-20 text-white">
+        <div class="flex flex-col items-start gap-6 max-w-md">
+
+          <img :src="logoBgDark" alt="logo" class="w-[260px]" />
+          <div>
+            <h2 class="text-3xl font-semibold mb-2">
+              Comece sua jornada literária
+            </h2>
+
+            <p class="leading-relaxed text-sm opacity-90">
+              Crie sua conta para avaliar livros, escrever resenhas
+              e compartilhar suas leituras favoritas.
+            </p>
+          </div>
+        </div>
+      </div>
+    </aside>
 
     <!-- RIGHT -->
-    <div class="flex-1 flex justify-center items-center bg-gray-100">
+    <main class="flex-1 flex justify-center items-center bg-white">
 
-      <div class="bg-white p-10 w-[420px] rounded-2xl shadow-xl">
+      <div class="flex flex-col items-center w-full">
 
-        <h2 class="text-xl font-semibold text-[#6B1E3D] mb-6">
-          Criar Conta
-        </h2>
+        <div class="mb-6 md:hidden">
+          <img :src="logoBgWhite" alt="Logo do StaReview" class="w-[180px]" />
+        </div>
 
-        <form class="space-y-4" @submit.prevent="handleRegister">
+        <div class="pt-10 pb-10 px-6 w-full max-w-[350px] rounded-2xl shadow-xl border border-red-400">
 
-          <!-- Nome -->
-          <div class="flex flex-col">
-            <label class="text-sm mb-1 text-gray-700">Nome</label>
-            <input
-              v-model="name"
-              v-bind="nameAttrs"
-              type="text"
-              placeholder="Seu nome completo"
-              class="p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#7E57C2]"
-            />
-            <p class="text-sm text-red-600">
+          <h2 class="flex text-xl font-semibold font-karla text-[#292929] justify-center mb-6">
+            Criar Conta
+          </h2>
+
+          <form class="space-y-4" @submit.prevent="handleRegister" novalidate>
+
+            <div class="flex flex-col">
+              <label for="name" class="text-sm mb-1 font-inconsolata text-gray-700 pl-3">Nome</label>
+              <input id="name" v-model="name" v-bind="nameAttrs" type="text" placeholder="Seu nome completo"
+                class="p-3 rounded-[25px] border bg-green-50 py-2 focus:outline-none" />
+              
+                <p class="text-sm text-red-600 mt-2">
                 {{ errors.name }}
+              </p>
+            </div>
+
+            <div class="flex flex-col">
+              <label for="email" class="text-sm mb-1 font-inconsolata text-gray-700 pl-3">Email</label>
+              <input id="email" v-model="email" v-bind="emailAttrs" type="email" placeholder="seuemail@email.com"
+                class="p-3 rounded-[25px] border bg-green-50 py-2 focus:outline-none" />
+              <p class="text-sm text-red-600">
+                {{ errors.email }}
+              </p>
+            </div>
+
+            <div class="flex flex-col">
+              <label for="password" class="text-sm mb-1 font-inconsolata text-gray-700 pl-3">Senha</label>
+              <input id="password" v-model="password" v-bind="passwordAttrs" type="password" placeholder="********"
+                class="p-3 rounded-[25px] border bg-green-50 py-2 focus:outline-none" />
+              <p class="text-sm text-red-600 mt-1">
+                {{ errors.password }}
+              </p>
+            </div>
+
+            <div class="flex flex-col">
+              <label for="confirmPassword" class="text-sm mb-1 font-inconsolata text-gray-700 pl-3">Confirmar Senha</label>
+              <input id="confirmPassword" v-model="confirmPassword" v-bind="confirmPasswordAttrs" type="password"
+                placeholder="********"
+                class="p-3 rounded-[25px] border bg-green-50 py-2 focus:outline-none" />
+
+              <p class="text-sm text-red-600 mt-1">
+                {{ errors.confirmPassword }}
+              </p>
+            </div>
+
+            <button type="submit" :disabled="loading"
+              class="w-full flex items-center justify-center bg-[#9BE9B2] text-[#292929] py-2 rounded-[25px] font-semibold hover:opacity-90 transition disabled:opacity-50">
+              {{ loading ? 'Cadastrando...' : 'Criar Conta' }}
+            </button>
+
+            <p class="text-center text-sm mt-4">
+              Já possui conta?
+              <router-link to="/login" class="text-[#880D1E] font-semibold">
+                Entrar
+              </router-link>
             </p>
-          </div>
 
-          <!-- Email -->
-          <div class="flex flex-col">
-            <label class="text-sm mb-1 text-gray-700">Email</label>
-            <input
-              v-model="email"
-              v-bind="emailAttrs"
-              type="email"
-              placeholder="seuemail@email.com"
-              class="p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#7E57C2]"
-            />
-            <p class="text-sm text-red-600">
-              {{ errors.email }}
-            </p>
-          </div>
-
-          <!-- Senha -->
-          <div class="flex flex-col">
-            <label class="text-sm mb-1 text-gray-700">Senha</label>
-            <input
-              v-model="password"
-              v-bind="passwordAttrs"
-              type="password"
-              placeholder="********"
-              class="p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#7E57C2]"
-            />
-            <p class="text-sm text-red-600 mt-1">
-              {{ errors.password }}
-            </p>
-          </div>
-
-          <!-- Confirmar -->
-          <div class="flex flex-col">
-            <label class="text-sm mb-1 text-gray-700">Confirmar Senha</label>
-            <input
-              v-model="confirmPassword"
-              v-bind="confirmPasswordAttrs"
-              type="password"
-              placeholder="********"
-              class="p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#7E57C2]"
-            />
-
-            <p class="text-sm text-red-600 mt-1">
-              {{ errors.confirmPassword }}
-            </p>
-          </div>
-
-          <!-- BOTÃO -->
-          <button
-            type="submit"
-            :disabled="loading"
-            class="w-full bg-[#6B1E3D] text-white py-3 rounded-lg font-semibold hover:opacity-90 transition disabled:opacity-50"
-          >
-            {{ loading ? 'Cadastrando...' : 'Criar Conta' }}
-          </button>
-
-          <!-- LINK -->
-          <p class="text-center text-sm mt-4">
-            Já possui conta?
-            <router-link to="/login" class="text-[#6B1E3D] font-semibold">
-              Entrar
-            </router-link>
-          </p>
-
-        </form>
+          </form>
+        </div>
       </div>
-
-    </div>
-
+    </main>
   </div>
 </template>
